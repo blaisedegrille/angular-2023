301c43c0701b79d0a90b4487c8c452885a7b9237
blaisedegrille <47615137+blaisedegrille@users.noreply.github.com>
2023-10-08 02:46:35 +0200

    feat: update base-edit-form

--- src/app/shared/base/base-edit-form/base-edit-form.component.ts
+++ src/app/shared/base/base-edit-form/base-edit-form.component.ts
@@ -1,10 +1,34 @@
-import { CdkStepper } from '@angular/cdk/stepper';
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'ang23-base-edit-form',
   templateUrl: './base-edit-form.component.html',
   styleUrls: ['./base-edit-form.component.scss'],
-  providers: [CdkStepper],
 })
-export class BaseEditFormComponent {}
+export class BaseEditFormComponent implements OnInit {
+  public form!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<BaseEditFormComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: User
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      id: this.fb.control(this.data.id, []),
+      name: this.fb.control(this.data.name, []),
+      email: this.fb.control(this.data.email, []),
+    });
+
+    console.log('this.form: ', this.form);
+    console.log('this.data: ', this.data);
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
