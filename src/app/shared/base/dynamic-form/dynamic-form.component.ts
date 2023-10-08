b7ea40a381f92b554209cd7149aed81ae8cd55df
blaisedegrille <47615137+blaisedegrille@users.noreply.github.com>
2023-10-08 02:46:57 +0200

    feat: generate dynamic-form component

---
+++ src/app/shared/base/dynamic-form/dynamic-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ang23-dynamic-form',
+  templateUrl: './dynamic-form.component.html',
+  styleUrls: ['./dynamic-form.component.scss']
+})
+export class DynamicFormComponent {
+
+}
