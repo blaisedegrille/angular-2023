a6f4047a6d9eefb1e015762e46413365b43480ee
blaisedegrille <47615137+blaisedegrille@users.noreply.github.com>
2023-10-09 17:55:02 +0200

    feat: firebase environment config

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,6 +2,12 @@ export const environment = {
   production: true,
   environmentName: 'QA',
   jsonPlaceholderApiUrl: 'https://jsonplaceholder.typicode.com',
-
   apiUrl: 'QA url',
+  apiKey: 'AIzaSyBlAtDNjjp49JdGiNI687twaaCQ2EBIa9c',
+  authDomain: 'angular-2023-bb816.firebaseapp.com',
+  projectId: 'angular-2023-bb816',
+  storageBucket: 'angular-2023-bb816.appspot.com',
+  messagingSenderId: '1090245849483',
+  appId: '1:1090245849483:web:2f2713aa29cc6ba381ff8e',
+  measurementId: 'G-165ERMPD4K',
 };
