11dab4c0a08a636ecf8db0179837d6b824472aa3
blaisedegrille <47615137+blaisedegrille@users.noreply.github.com>
2023-10-13 16:49:15 +0200

    feat: firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,13 @@ export const environment = {
   environmentName: '',
   jsonPlaceholderApiUrl: '',
   apiUrl: '',
+  firebase: {
+    apiKey: 'AIzaSyBlAtDNjjp49JdGiNI687twaaCQ2EBIa9c',
+    authDomain: 'angular-2023-bb816.firebaseapp.com',
+    projectId: 'angular-2023-bb816',
+    storageBucket: 'angular-2023-bb816.appspot.com',
+    messagingSenderId: '1090245849483',
+    appId: '1:1090245849483:web:2f2713aa29cc6ba381ff8e',
+    measurementId: 'G-165ERMPD4K',
+  },
 };
