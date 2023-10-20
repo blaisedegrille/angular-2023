7509f3e883bdd29be912dc8c8d680e6e22bfef91
blaisedegrille <47615137+blaisedegrille@users.noreply.github.com>
2023-10-20 21:01:46 +0200

    feat: close event

--- src/app/pages/components/dynamic-grid/dynamic-grid.component.ts
+++ src/app/pages/components/dynamic-grid/dynamic-grid.component.ts
@@ -1,3 +1,4 @@
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DisplayedColumns } from 'src/app/models/displayed-columns';
@@ -26,15 +27,15 @@ export class DynamicGridComponent extends BaseGridComponent<User> {
         header: 'Email',
       },
     ],
-    columns: ['id', 'name', 'email', 'select'],
+    columns: ['select', 'id', 'name', 'email', 'action'],
     // headers: ['No.', 'Title', 'Body', 'UserId'],
   };
 
   constructor(
     private readonly userService: UserService,
-    public override dialog: MatDialog
+    public override dialog: MatDialog //public override _liveAnnouncer: LiveAnnouncer
   ) {
-    super(dialog);
+    super(dialog /* _liveAnnouncer */);
     super.service = userService;
     super.displayedColumns = this.cols;
   }
