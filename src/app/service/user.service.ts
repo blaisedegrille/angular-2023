d5885493956b6c16e3d0def267d2993c98a6242a
blaisedegrille <47615137+blaisedegrille@users.noreply.github.com>
2023-10-20 20:59:55 +0200

    feat: update service

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -22,28 +22,53 @@ export class UserService {
   private tmp: User[] = [
     {
       id: 1,
-      name: 'Test User 1',
+      name: 'User 1',
       email: 'test1@email.com',
     },
     {
       id: 2,
-      name: 'Test User 2',
+      name: 'User 2',
       email: 'test2@email.com',
     },
     {
       id: 3,
-      name: 'Test User 3',
-      email: 'test3@email.com',
+      name: 'User 3',
+      email: 'test3@1email.com',
     },
     {
       id: 4,
-      name: 'Test User 4',
-      email: 'test4@email.com',
+      name: 'User 4',
+      email: 'test4@1email.com',
     },
     {
       id: 5,
-      name: 'Test User 5',
+      name: 'User 5',
       email: 'test5@email.com',
+    },
+    {
+      id: 6,
+      name: ' User 1_2',
+      email: 'test1@email2.com',
+    },
+    {
+      id: 7,
+      name: ' User 2_2',
+      email: 'test2@email2.com',
+    },
+    {
+      id: 8,
+      name: ' User 3_2',
+      email: 'test3@email3.com',
+    },
+    {
+      id: 9,
+      name: ' User 4_2',
+      email: 'test4@email3.com',
+    },
+    {
+      id: 10,
+      name: ' User 5_2',
+      email: 'test5@1email.com',
     },
   ];
 }
